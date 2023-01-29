d494d4c63ea512c721ed6b1f798026c7e525ae2f
Ryzhenko Oleksii <116575138+Ryzh313@users.noreply.github.com>
2023-01-29 18:08:47 +0200

    update1

--- src/js/refs-elements.js
+++ src/js/refs-elements.js
@@ -1,5 +1,5 @@
 export const refs = {
-  searchBox: document.getElementById('search-box'),
-  countryList: document.querySelector('ul.country-list'),
-  countryInfo: document.querySelector('div.country-info'),
+  searchBox: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
 };
